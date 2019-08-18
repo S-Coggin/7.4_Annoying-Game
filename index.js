47d467772d3161fa5bb68a07fea1149ed822fc69
S-Coggin <53793753+S-Coggin@users.noreply.github.com>
2019-08-18 16:11:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// alert("CONNECTED");
+// var answer = prompt("Are we there yet?");
+
+// while(answer !=="yes" && answer !== "yeah") {
+//     var answer = prompt("Are we there yet?");
+// }
+// alert("Yay, we finally made it!");
